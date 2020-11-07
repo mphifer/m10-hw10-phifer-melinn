d9003785dc2a5122104851d728e51c0b7299a1c3
Melinn Phifer <mphifer@ufl.edu>
2020-11-07 16:21:44 -0500

    add if/else statement to JS

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # m10-hw10-phifer-melinn
+
+
+https://www.w3schools.com/js/js_arrow_function.asp
+
+
+https://www.w3schools.com/jsref/jsref_if.asp
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+
+Had trouble setting up setTimeout for the number > 32 but < 212.
--- src/main.js
+++ src/main.js
@@ -1,2 +1,34 @@
 // It is always helpful to use comments in your code!
 console.log('test');
+
+const boilingWater = () => {
+  document.getElementById("boiling-temp").innerHTML = "212";
+};
+boilingWater();
+
+const freezingWater = () => {
+  document.getElementById("freezing-temp").innerHTML = "32";
+};
+freezingWater();
+
+const currentWater = () => {
+  document.getElementById("water-temp").innerHTML = "98";
+};
+currentWater();
+
+let myTemp = 98;
+
+setTimeout(function waterTemps() {
+  if (myTemp > 212) {
+    document.getElementById("boiling").classList.remove("hide");
+    document.getElementById("boiling").classList.add("show");
+    }
+  else if (myTemp < 32) {
+    document.getElementById("frozen").classList.remove("hide");
+    document.getElementById("frozen").classList.add("show");
+    }
+  else {
+    document.getElementById("good-temp").classList.remove("hide");
+    document.getElementById("good-temp").classList.add("show");
+    }
+}, 3000);
